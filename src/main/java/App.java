346a530b8eb1809dba56791923e07edd99f1d8a5
Your Name <you@example.com>
2017-03-19 22:13:14 +0200

    bashka ne varit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,43 +11,89 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        float f1= (float) 3.14;
+        float f1 = (float) 3.14;
         System.out.println(f1);
-        long i1=2000000007204545454L;
+        long i1 = 2000000007204545454L;
         System.out.println(i1);
         Random r;
         r = new Random();
         System.out.println(r.nextLong());
 
-        int count=0;
+        int count = 0;
 
-        for(int i=1; i<20; i=i+1){
-            int i2=r.nextInt();
-            if (i2%2==1) {
+        for (int i = 1; i < 20; i = i + 1) {
+            int i2 = r.nextInt();
+            if (i2 % 2 == 1) {
                 System.out.print("1");
-            }
-            else {
-                count=count+1;
+            } else {
+                count = count + 1;
                 System.out.print("0");
-                if (count==10)
+                if (count == 10)
                     break;
 
             }
         }
 
-/*        int i2=r.nextInt();
-        if (i2%2==1) {
+        int i2 = r.nextInt();
+        if (i2 % 2 == 1) {
             System.out.println("1");
-        }
-        else {
+        } else {
             System.out.println("0");
-        }*/
+        }
+
+        String s = new String("hello");
+        System.out.println(s);
+        char[] charArray = s.toCharArray();
+        int size = s.length();
+        for (int i = 0; i < size / 2; i++) {
+            int x = size - i - 1;
+            swap(charArray, i, x);
+
+        }
+        System.out.println(charArray);
+
+        int[] a = {0, 1, 2, 3, 4, 5};
+        for (int i = 0; i < a.length / 2; i++) {
+            int x = i + a.length / 2;
+            swap(a, i, x);
+        }
+        printArray(a);
+
+
+
+
     }
+
+    private static void printArray(int[] a) {
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i]);
+        System.out.println();
+    }
+
+
+    private static void swap(int[] charArray, int i, int x) {
+        int buffer = charArray[i];
+        charArray[i] = charArray[x];
+        charArray[x] = buffer;
+
+
+    }
+
+
+    private static void swap(char[] charArray, int i, int x) {
+        char buffer = charArray[i];
+        charArray[i] = charArray[x];
+        charArray[x] = buffer;
+
+
+    }
+
+
 }
 
 class App2 {
     int kvadrat(int valuekvadrat) {
-    int resultat = valuekvadrat*valuekvadrat;
+        int resultat = valuekvadrat * valuekvadrat;
         return resultat;
     }
 }
